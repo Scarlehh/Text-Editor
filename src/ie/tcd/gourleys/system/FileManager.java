9ab80a416ac7cebb903590da3fe2783a29362315
Scarlett <gourleys@tcd.ie>
2016-10-12 18:44:29 +0100

    Cleaned up file management

--- src/ie/tcd/gourleys/system/FileManager.java
+++ src/ie/tcd/gourleys/system/FileManager.java
@@ -1,113 +1,83 @@
 package ie.tcd.gourleys.system;
 
-import ie.tcd.gourleys.Main;
-import ie.tcd.gourleys.ui.TextEditorWindow;
+import ie.tcd.gourleys.ui.Window;
 
-import java.awt.FileDialog;
-import java.awt.TextArea;
-import java.io.*;
+import javax.swing.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 
 public class FileManager {
-	
-	public static final String DEFAULT_PATH = "C:/Users/Scarlett/Desktop/";
-	public static final String POSTFIX = ".scrub";
+	String name;
+	String dir;
 
-	/* ====================================================================
-	 * 		Open + Save files
-	 * ====================================================================	
-	 */
+	public FileManager() {
+		this(null, null);
+	}
 	
-	public static TextContainer open(TextEditorWindow window) {
-		FileDialog dialog = new FileDialog(window, "Open File", FileDialog.LOAD);
-		dialog.setVisible(true);
-		String title = dialog.getFile();
-		if (title != null) {
-			String path = dialog.getDirectory() + title;
-			System.out.println("File selected: " + path);
-			String text = loadFile(path);
-			System.out.println("File loaded successfully.");
-			return new TextContainer(title, text);
+	public FileManager(String name, String dir) {
+		this.name = name;
+		this.dir = dir;
+	}
+	
+	public void openFile(JFrame window) {
+		JFileChooser chooser = new JFileChooser();
+		if(dir != null) {
+			chooser.setCurrentDirectory(new File(dir));
 		}
-		System.out.println("No file selected.");
+		chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		if(chooser.showOpenDialog(window) == JFileChooser.APPROVE_OPTION) {
+			File file = chooser.getSelectedFile();
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader(file));
+				String line = null;
+				String in = "";
+				try {
+					while((line = reader.readLine()) != null) {
+						in += line + "\n";
+					}
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+				new Window(chooser.getName(file), in, chooser.getCurrentDirectory().getPath());
+			} catch(FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public String saveFile(JFrame window, String file) {
+		JFileChooser chooser = new JFileChooser();
+		chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		if(chooser.showSaveDialog(window) == JFileChooser.APPROVE_OPTION) {
+			try {
+				FileWriter writer = new FileWriter(chooser.getSelectedFile());
+				writer.write(file);
+				writer.close();
+				name = chooser.getName(chooser.getSelectedFile());
+				dir = chooser.getCurrentDirectory().getPath();
+				return name;
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
 		return null;
 	}
-	
-	public static void save(TextEditorWindow window) {
-		FileDialog dialog = new FileDialog(window, "Save File", FileDialog.SAVE);
-		dialog.setVisible(true);
-		String title = dialog.getFile();
-		if (title != null) {
-			if (!title.endsWith(POSTFIX)) {
-				title += POSTFIX;
-			}
-			window.setTitle(title);
-			String path = dialog.getDirectory() + title;
-			System.out.println("Path selected: " + path);
-			createFile(window.getText(), path);
-			System.out.println("Successfully created file.");
-		}
-	}
-	
-	public static void quicksave(TextEditorWindow window) {
-		String title = window.getTitle();
-		if (!title.endsWith(POSTFIX)) {
-			title += POSTFIX;
-			window.setTitle(title);
-		}
-		String path = DEFAULT_PATH + title;
-		System.out.println("Path selected: " + path);
-		createFile(window.getText(), path);
-		System.out.println("Successfully created file.");
-	}
 
-	/* ====================================================================
-	 * 		get and set files
-	 * ====================================================================	
-	 */
-	
-	public static String loadFile(String path) {
-		String text = "";
-		try {
-			File file = new File(path);
-			FileReader filereader = new FileReader(file);
-			BufferedReader bufferedreader = new BufferedReader(filereader);
-			while (bufferedreader.ready()) {
-				text += bufferedreader.readLine() + "\n";
-			}
-			bufferedreader.close();
-			if (text == "") {
-				System.out.println("Invalid file.");
-			}
-		} catch (FileNotFoundException e1) {
-			System.out.println("File does not exist.");
-		} catch (IOException e) {
-			System.out.println("Input Error.");
+	public String quickSave(JFrame window, String file) {
+		if(name == null || dir == null) {
+			return saveFile(window, file);
 		}
-		return text;
-	}
-	
-	public static File createFile(TextArea text, String path) {
-		File file = new File(path);
 		try {
-			FileWriter filewriter = new FileWriter(file);
-			BufferedWriter bufferedwriter = new BufferedWriter(filewriter);
-			bufferedwriter.write(text.getText());
-			bufferedwriter.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			FileWriter writer = new FileWriter(new File(dir+"/"+name));
+			writer.write(file);
+			writer.close();
+		} catch(IOException e) {
 			e.printStackTrace();
 		}
-		return file;
+		return name;
 	}
-
-
-	/* ====================================================================
-	 * 		main()
-	 * ====================================================================	
-	 */
-	
-	public static void main(String[] args) {
-		Main.main(args);
-	}
-
 }
