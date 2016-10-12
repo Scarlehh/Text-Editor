6033a3c8b55fb753c344f74d46059fb656f917bf
Scarlett <gourleys@tcd.ie>
2016-10-12 18:43:41 +0100

    Only calls window

--- src/ie/tcd/gourleys/Main.java
+++ src/ie/tcd/gourleys/Main.java
@@ -1,55 +1,11 @@
 package ie.tcd.gourleys;
 
-import java.util.ArrayList;
-
-import ie.tcd.gourleys.system.*;
 import ie.tcd.gourleys.ui.*;
-import ie.tcd.gourleys.ui.listener.*;
 
 public class Main {
-	static ArrayList<TextEditorWindow> windowList;
-	
-	public static void addWindow(ArrayList<TextEditorWindow> windowList, TextContainer contents) {
-		int offset = windowList.size() * 50;
-		if (contents != null) {
-			TextEditorWindow window = new TextEditorWindow(offset, offset, contents);
-			window.addWindowListener(new TextWindowListener());
-			window.setVisible(true);
-			windowList.add(window);
-		}
-	}
-	
-	public static void addWindow(ArrayList<TextEditorWindow> windowList) {
-		addWindow(windowList, null);
-	}
-	
-	/* ====================================================================
-	 * 		main()
-	 * ====================================================================	
-	 */
-	
-	public static void main(String[] args) {
-		windowList = new ArrayList<TextEditorWindow>();
-		Main.addWindow(windowList, new TextContainer());
 
-		while (windowList.size() != 0) {
-			for (int i = 0; i < windowList.size(); i++) {
-				if (windowList.get(i).isNewWindow()) {
-					windowList.get(i).setNewWindow(false);
-					Main.addWindow(windowList, new TextContainer());
-				}
-				if (windowList.get(i).isOpenWindow()) {
-					windowList.get(i).setOpenWindow(false);
-					Main.addWindow(windowList, windowList.get(i).getOpen());
-				}
-				
-				// Removes window from array list if disposed.
-				if (!windowList.get(i).isDisplayable()) {
-					windowList.remove(i);
-				}
-			}
-		}
-		System.out.println("All windows are closed.");
+	public static void main(String[] args) {
+		new Window();
 	}
 
 }
